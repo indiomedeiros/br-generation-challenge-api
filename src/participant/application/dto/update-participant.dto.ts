14dbf65c09488dd46ecab0951fe865d82b8fc9c7
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2025-06-29 03:51:56 -0300

    refactor|fix: implementa atualização de media final no endpoint de atualização de participant e valida o null

--- src/participant/application/dto/update-participant.dto.ts
+++ src/participant/application/dto/update-participant.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateParticipantDto } from './create-participant.dto';
 
-export class UpdateParticipantDto extends PartialType(CreateParticipantDto) {}
+export class UpdateParticipantDto extends PartialType(CreateParticipantDto, {
+  skipNullProperties: false,
+}) {}
--- src/participant/application/usecases/update-participant.usecase.ts
+++ src/participant/application/usecases/update-participant.usecase.ts
@@ -2,10 +2,14 @@ import { Participant } from '../../domain/entities/participant.entity';
 import { UpdateParticipantDto } from '../dto/update-participant.dto';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ParticipantRepository } from '../../domain/repositories/participant-repository';
+import { GradeCalculatorService } from '../../domain/services/grade-calculator.service';
 
 @Injectable()
 export class UpdateParticipantUseCase {
-  constructor(private readonly participantRepository: ParticipantRepository) {}
+  constructor(
+    private readonly participantRepository: ParticipantRepository,
+    private readonly gradeCalculatorService: GradeCalculatorService,
+  ) {}
 
   async execute(
     id: string,
@@ -20,6 +24,24 @@ export class UpdateParticipantUseCase {
       existingParticipant,
       updateParticipantDto,
     );
+
+    if (
+      updateParticipantDto.firstSemesterGrade !== undefined ||
+      updateParticipantDto.secondSemesterGrade !== undefined
+    ) {
+      const firstGrade: number =
+        updateParticipantDto.firstSemesterGrade ??
+        existingParticipant.firstSemesterGrade;
+
+      const secondGrade: number =
+        updateParticipantDto.secondSemesterGrade ??
+        existingParticipant.secondSemesterGrade;
+
+      const updatedFinalMedia: number =
+        this.gradeCalculatorService.calculateAverage(firstGrade, secondGrade);
+
+      updatedParticipant.finalAverage = updatedFinalMedia;
+    }
 
     return await this.participantRepository.save(updatedParticipant);
   }
