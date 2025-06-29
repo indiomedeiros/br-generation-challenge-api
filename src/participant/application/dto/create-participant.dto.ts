6ab6078e6298f235bb6fd02f45b03f7cb3392ba0
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2025-06-29 00:46:22 -0300

    feat|refactor: faz a validação do endpoint de criação de participant

--- src/participant/application/dto/create-participant.dto.ts
+++ src/participant/application/dto/create-participant.dto.ts
@@ -1,6 +1,35 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
 export class CreateParticipantDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   readonly fullName: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(18)
+  @Max(30)
   readonly age: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(10)
   readonly firstSemesterGrade: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(10)
   readonly secondSemesterGrade: number;
 }
