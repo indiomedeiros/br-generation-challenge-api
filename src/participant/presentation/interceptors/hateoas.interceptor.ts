cf7c4f6c912212283b35b9d7aeabd214ba3406c9
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2025-06-30 20:57:35 -0300

    fix: corrige a relação de links enviado no interceptador da resposta

--- src/participant/presentation/interceptors/hateoas.interceptor.ts
+++ src/participant/presentation/interceptors/hateoas.interceptor.ts
@@ -7,43 +7,51 @@ import { Participant } from 'src/participant/domain/entities/participant.entity'
 export class HateoasInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest<HateoasRequest>();
-
-    const baseUrl: string = request.host;
-    const hasID: string | undefined = request.params.id;
-    const method: string = request.method;
+    const { host, method } = request;
     const _links: HateoasLink[] = [];
 
     if (method === 'DELETE') return next.handle();
 
-    if (hasID) {
-      _links.push(
-        {
-          rel: 'self',
-          href: `http://${baseUrl}/participant/${hasID}`,
-          method: 'GET',
-        },
-        {
-          rel: 'update',
-          href: `http://${baseUrl}/participant/${hasID}`,
-          method: 'PATCH',
-        },
-        {
-          rel: 'delete',
-          href: `http://${baseUrl}/participant/${hasID}`,
-          method: 'DELETE',
-        },
-      );
-    } else {
-      _links.push(
-        { rel: 'list', href: `http://${baseUrl}/participant`, method: 'GET' },
-        {
-          rel: 'create',
-          href: `http://${baseUrl}/participant`,
-          method: 'POST',
-        },
-      );
-    }
+    const baseUrl = `http://${host}/participant`;
 
-    return next.handle().pipe(map((data: Participant) => ({ data, _links })));
+    return next.handle().pipe(
+      map((data: Participant) => {
+        const hasId = data.id;
+        if (data.id) {
+          _links.push(
+            {
+              rel: 'self',
+              href: `${baseUrl}/${hasId}`,
+              method: method,
+            },
+            {
+              rel: 'get',
+              href: `${baseUrl}/${hasId}`,
+              method: 'GET',
+            },
+            {
+              rel: 'update',
+              href: `${baseUrl}/${hasId}`,
+              method: 'PATCH',
+            },
+            {
+              rel: 'delete',
+              href: `${baseUrl}/${hasId}`,
+              method: 'DELETE',
+            },
+          );
+        } else {
+          _links.push(
+            { rel: 'list', href: `${baseUrl}`, method: 'GET' },
+            {
+              rel: 'create',
+              href: `${baseUrl}`,
+              method: 'POST',
+            },
+          );
+        }
+        return { data, _links };
+      }),
+    );
   }
 }
